e9a0966f8874b314f1e613e28fee652e8d899194
Isa Medina <isamed92@gmail.com>
2021-12-12 23:33:47 -0300

    use Memo added

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../../helpers/getHeroById';
 
@@ -6,7 +7,7 @@ export const HeroScreen = () => {
 
   const navigate = useNavigate();
 
-  const hero = getHeroById(id);
+  const hero = useMemo(()=>getHeroById(id), [id]);
 
   if (!hero) {
     return <Navigate to='/' />;
