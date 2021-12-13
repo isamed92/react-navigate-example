e9b56acb133b43a7918544c49f061794d3a217d1
Isa Medina <isamed92@gmail.com>
2021-12-13 04:22:27 -0300

    useForm hook added

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -26,7 +26,7 @@ export const HeroScreen = () => {
   return (
     <div className='row mt-5'>
       <div className='col-4'>
-        <img src={imgPath} alt={superhero} className='img-thumbnail' />
+        <img src={imgPath} alt={superhero} className='img-thumbnail  animate__animated animate__fadeInLeft' />
       </div>
       <div className='col-8'>
         <h3>{superhero}</h3>
--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from '../../hooks/useForm';
 
 export const SearchScreen = () => {
-    return (
-        <div>
-            <h1>SearchScreen</h1>
+  const [formValues, handleInputChange] = useForm({
+    search: '',
+  });
+
+  const { search } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
+
+  return (
+    <>
+      <h1>Busquedas</h1>
+      <hr />
+      <div className='row'>
+        <div className='col-5'>
+          <h4>Buscar</h4>
+          <hr />
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              placeholder='Buscar un heroe'
+              className='form-control'
+              name='search'
+              autoComplete='off'
+              value={search}
+              onChange={handleInputChange}
+            />
+            <button className='btn btn-outline-primary mt-1' type='submit'>
+              Buscar
+            </button>
+          </form>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
