8d4e4cfce4eaf63749c91cc33195bb07e3d303b2
Isa Medina <isamed92@gmail.com>
2021-12-12 23:26:57 -0300

    useNavigate and more

---
+++ src/components/hero/HeroScreen.js
@@ -0,0 +1,51 @@
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../../helpers/getHeroById';
+
+export const HeroScreen = () => {
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  const hero = getHeroById(id);
+
+  if (!hero) {
+    return <Navigate to='/' />;
+  }
+
+  const { superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+
+  const imgPath = `/assets/${hero.id}.jpg`;
+
+  const handleOnClick = () => {
+    //   publisher === 'Marvel Comics' ? navigate('/') : navigate('/dc')
+    navigate(-1)
+  }
+
+  return (
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img src={imgPath} alt={superhero} className='img-thumbnail' />
+      </div>
+      <div className='col-8'>
+        <h3>{superhero}</h3>
+        <ul className='list-group-flush'>
+          <li className='list-group-item'>
+            <b>Alter Ego: </b> {alter_ego}
+          </li>
+          <li className='list-group-item'>
+            <b>Publisher: </b> {publisher}
+          </li>
+          <li className='list-group-item'>
+            <b>First Appearance: </b> {first_appearance}
+          </li>
+        </ul>
+        <h5 className='mt-3'>Characters</h5>
+        <p>{characters}</p>
+        <button className='btn btn-primary' onClick={handleOnClick} >
+            Regresar
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ src/helpers/getHeroesByPublisher.js
@@ -0,0 +1,13 @@
+import { heroes } from "../data/heroes"
+
+
+export const getHeroesByPublisher = (publisher) => {
+    const validPublishers  = ['DC Comics', 'Marvel Comics'];
+    if( !validPublishers.includes(publisher) ){
+        throw new Error(`${publisher} no es una empresa válida`);
+    }
+
+
+    return heroes.filter(hero => hero.publisher === publisher)
+
+}
